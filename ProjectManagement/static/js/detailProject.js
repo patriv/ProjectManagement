31efd484a67ee264f6d01978f66cacc6d33c2465
patriv <patty.vl91@gmail.com>
2017-08-21 17:43:16 -0400

    Arreglo menores

--- ProjectManagement/static/js/detailProject.js
+++ ProjectManagement/static/js/detailProject.js
@@ -1,18 +1,15 @@
 $(document).ready(function (){
-    var elems = $(".userProject").length;
-    alert(elems);
-    if (elems > 3){
-        var $newvisibility = $('<i class = "mdi-navigation-more-horiz" id = "viewTask"></i></a>');
-        alert("mayor");
-        $(".recent-activity-list-text").append($newvisibility);
-        for(var i = 0; ele = $('.userProject')[i]; i++) {
+    var elems = $(".userProject");
+    var $newvisibility = $('<i class = "mdi-navigation-more-horiz" id = "viewTask"></i></a>');
+    alert("mayor");
+    $(".recent-activity-list-text").append($newvisibility);
+    for(var i = 0; ele = elems[i]; i++) {
+        alert(i);
+        if (i > 2) {
             alert(i);
-            if (i > 2) {
-                alert(i);
-                if (ele.className === 'userProject')
-                    ele.innerHTML = '';
-            }
+            if (ele.className === 'userProject')
+                ele.innerHTML = '';
         }
-        //$(".recent-activity-list-text").toggle();
     }
+    //$(".recent-activity-list-text").toggle();
 });
